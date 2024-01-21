507d8598d9a502ea7833c32838710fea1d80fb6d
penguinumbrella <ginapeng2002@gmail.com>
2024-01-21 07:54:47 -0800

    merged

--- server.js
+++ server.js
@@ -2,25 +2,55 @@ const express = require("express");
 const webpush = require("web-push");
 const bodyParser = require("body-parser");
 const path = require("path");
-
+const {MongoClient} = require("mongodb");
+const user = require("./routes/user.js");
 const app = express();
+
+app.use(express.static(path.join(__dirname, "client")));
+
+app.use(bodyParser.json());
+
+
 app.use(express.json());
 const port = 3000;
 const uri = 'mongodb://127.0.0.1:27017'; // Replace with your MongoDB connection string
 const client = new MongoClient(uri);
 
+
+const publicVapidKey =
+  "BJ8NBEgdl89k71YbzUGwKAYdbZF97XobHNUlNiX8xjDD5YEHkXxbv_JrC7pgkPAZkiFtdqy6EY7JKmn9NQJcitY";
+const privateVapidKey = "3AysE1150GFoHijLeC3mmJGftO0cZGROeSiTalMiSpE";
+
+webpush.setVapidDetails(
+    "mailto:test@test.com",
+    publicVapidKey,
+    privateVapidKey
+  );
+
+  // Subscribe Route
+app.post("/subscribe", (req, res) => {
+  // Get pushSubscription object
+  const subscription = req.body;
+
+  // Send 201 - resource created
+  res.status(201).json({});
+
+  // Create payload
+  const payload = JSON.stringify({ title: "Push Test" });
+
+  // Pass object into sendNotification
+  webpush
+    .sendNotification(subscription, payload)
+    .catch(err => console.error(err));
+});
+
+app.use(express.static('public'));
+
+
+
+
 //const user = require('./routes/user.js');
 
-// Connect to the MongoDB database before starting the server
-async function connectToDatabase() {
-  try {
-    await client.connect();
-    console.log('Connected to the database');
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-    process.exit(1); // Terminate the process if unable to connect to the database
-  }
-}
 
 
 app.get('/', async (req, res) => {
@@ -28,10 +58,46 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/db/userlist', user.createNewUser);
+app.put('/db/userlist/:username', user.updateUser);
+
+app.post('/db/userlist/:username/feelings', user.addFeelings);
+app.get('/db/userlist/:username/name', user.getName);
+app.get('/db/userlist/:username/feelings', user.getFeelings);
+
+// working
+app.post('/db/userlist/:username/activity', user.addActivity);
+
+// editActivity:
+// issue: some activities have multiple words, but the endpoint URL only accepts one continous word for the activity
+// OK: ".../exampleUser/activity"
+// NOT OK: ".../exampleUser/multiple word activity"
+// this is working for single word activities only
+app.put('/db/userlist/:username/:activity', user.editActivity);
+
+
+app.delete('/db/userlist/:username/:activity', user.removeActivity);
+
+app.get('/db/userlist/:username/:soreArea', user.getSoreArea);
 
 // Start the server after connecting to the database
-connectToDatabase().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-  });
-});
+async function connectToDatabase() {
+    try {
+      await client.connect();
+      console.log('Connected to the database');
+    } catch (error) {
+      console.error('Error connecting to the database:', error);
+      process.exit(1); // Terminate the process if unable to connect to the database
+    }
+  }
+  
+  
+  app.get('/', async (req, res) => {
+      res.sendFile(path.join(__dirname, 'index.html'));
+    });
+    
+    // Start the server after connecting to the database
+    connectToDatabase().then(() => {
+      app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+      });
+    });
