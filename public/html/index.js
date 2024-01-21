19b0c1f6318d72d6abf083ed8131f9b57be7ac24
penguinumbrella <ginapeng2002@gmail.com>
2024-01-21 11:34:54 -0800

    fin???

--- public/html/index.js
+++ public/html/index.js
@@ -2,7 +2,7 @@ import {createChat} from "./instructions.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    document.getElementById('generateResponseButton').addEventListener('click', () => {
+    document.getElementById('nextBtn').addEventListener('click', () => {
         // const inputText = document.getElementById('text-prompt').value;
         const instructions = "arms";
         console.log("Input text:", instructions);
